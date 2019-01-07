792753b88e35f71610d540fc2c75cffc4617b652
Max Carlquist <tenkaklet@gmail.com>
2019-01-07 13:56:29 +0100

    changed to es6

---
+++ app.js
@@ -0,0 +1,28 @@
+// String
+const name = 'Max';
+// Number
+const number = 500;
+
+// Null
+const houseSomeday = null;
+
+// Undefined
+const walking = undefined;
+
+// Boolean
+const lazy = true;
+
+// Array
+const shoppingList = [name, number, houseSomeday, walking, lazy];
+console.log(person);
+console.log(shoppingList);
+// Object
+const person = {
+    name: name,
+    pay: number,
+    house: houseSomeday,
+    excersise: walking,
+    lazy: lazy
+};
+
+console.log(person);
